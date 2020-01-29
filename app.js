8039da5d2b93e86379992b0f4683e9ad636628e1
Syahnisura Sitepu <syahnisura12@gmail.com>
2020-01-29 09:35:15 +0700

    [Syahnisura] - create todolist

---
+++ app.js
@@ -0,0 +1,54 @@
+Pratur Anahatta, [29.01.20 09:25]
+const input = document.querySelector(".inputin");
+const addBtn = document.querySelector(".tambah");
+// const deletelist = document.querySelector(".hapus");
+const listnya=document.querySelector(".list");
+
+input.addEventListener("keyup", takeValue);
+addBtn.addEventListener("click", addList);
+// deletelist.addEventListener("click", deleteLi);
+
+listnya.addEventListener('click', function(e){
+    const test = e.target;
+    const a = test.getAttribute("class");
+    // console.log(listnya.childNodes[test]);
+    if (a === "hapus"){
+        // console.log(a);
+        listnya.removeChild(listnya.childNodes[0]); 
+
+    }
+    
+        
+
+})
+
+
+let isi=" ";
+function takeValue(e) {
+    let text=e.target.value;
+    isi=text;    
+}
+
+function addList(e) {
+    e.preventDefault()
+    // console.log(isi)
+    const el = document.createElement("li");
+    const text = document.createTextNode(isi);
+    const hapus = document.createElement("button");
+    const texthapus = document.createTextNode("x");
+
+
+
+    el.appendChild(text);
+    hapus.appendChild(texthapus);
+    hapus.classList.add("hapus");
+    el.appendChild(hapus);
+    listnya.appendChild(el);
+    
+    // listnya.appendChild(hapus);
+    
+}
+
+// function deleteLi(e) {
+//     console.log("hey")
+// }
